2e5447fcd61b206694a5ea95c9343b3499b19096
Jermaine Janszen <jermainejanszen@gmail.com>
2020-08-05 16:01:28 +1000

    added nav bar to homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {Route, BrowserRouter as Router, Link} from "react-router-dom";
-import Nav from './Nav'; 
-
-import '../styles/App.css';
+import { Provider as BumbagProvider, PageWithHeader } from 'bumbag';
+import Nav from './Nav';
+import PostList from './PostList';
 
 function App() {
     return (
         <Router>
-            <Nav />
+            <BumbagProvider>
+                <PageWithHeader
+                    header={ <Nav /> }
+                    border="default"
+                >
+                    <PostList />
+                </PageWithHeader>
+            </BumbagProvider>
         </Router>
     );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { TopNav, Image, Button } from 'bumbag';
 
 export default class Nav extends Component {
     render() {
         return (
-            <div className="nav">
-                <Link to="/">Home</Link>
-                <br/>
-                <Link to="/play">Play</Link>
-            </div>
+            <TopNav>
+                <TopNav.Section>
+                    <TopNav.Item href="/" fontWeight="semibold">
+                        <Image src="/logo.png" height="44px" />
+                    </TopNav.Item>
+                    <TopNav.Item href="#">Main Post</TopNav.Item>
+                    <TopNav.Item href="#">Comments</TopNav.Item>
+                </TopNav.Section>
+                <TopNav.Section marginRight="major-2">
+                    <TopNav.Item>
+                        <Button variant="ghost" palette="primary">Save</Button>
+                    </TopNav.Item>
+                    <TopNav.Item>
+                        <Button variant="ghost" palette="primary">Load</Button>
+                    </TopNav.Item>
+                    <TopNav.Item>
+                        <Button palette="primary">Play</Button>
+                    </TopNav.Item>
+                </TopNav.Section>
+            </TopNav>
         )
     }
 }
